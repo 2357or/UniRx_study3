3efa1b4df17437ba1e83fef1a01289f01b6d707f
大槻亮太 <ootsukiryouta@ootsukiryoutanoMacBook-Pro.local>
2020-04-21 11:43:45 +0900

    ファイルを追加

---
+++ CarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+
+public class CarController :MonoBehaviour
+{
+    [SerializeField]
+    float speed = 20, deceleration=0.98f;
+
+    void Start() {
+        this.UpdateAsObservable()
+            .SkipWhile(_ => !Input.GetMouseButtonDown(0))
+            .Subscribe(_ =>
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+                speed *= deceleration;
+            });
+    }
+}
